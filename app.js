8df095a60810980c89e735f80a21a5cdf276fc3f
Kevin Adrian <adrian.kevin@yahoo.com>
2023-04-25 22:54:37 +0800

    Update procfile

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname+"/public"))
 
-mongoose.connect("mongodb://127.0.0.1:27017/todolistDB");
+mongoose.connect("mongodb+srv://admin-kevin:Test123456@cluster0.6ehnxuj.mongodb.net/todolistDB");
 
 const itemsSchema = new mongoose.Schema({
     itemName: String
