f7d993a19a29dd3d482a3e9054e061066afcf752
Kevin Adrian <adrian.kevin@yahoo.com>
2023-04-25 23:31:04 +0800

    fix port and package

--- app.js
+++ app.js
@@ -115,11 +115,6 @@ app.get("/about", function(req,res){
     res.render("about");
 })
 
-let port =process.env.PORT;
-if(port == null || port ===""){
-    port = 3000;
-}
-
-app.listen(port, function () {
+app.listen(process.env.PORT || 3000, function () {
     console.log("Server started on port "+ port);
 });
